7bf38a1ac283d89b5726fa6646f61251ae543b48
babarparacha <babarmuzaffar3@gmail.com>
2025-04-18 21:25:50 -0700

    first commit

---
+++ src/constants.js
@@ -0,0 +1,293 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import gads from './assets/tech_logo/gAds.png';
+import metaAds from './assets/tech_logo/metaAds.png';
+import seo from './assets/tech_logo/seo.png';
+import semrush from './assets/tech_logo/semrush.png';
+import ahref from './assets/tech_logo/ahref.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+import college from './assets/education_logo/college.png';
+import superior from './assets/education_logo/superior.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+import aiGen from './assets/work_logo/aiGen.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Laravel', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Digital Marketing',
+    skills: [
+      { name: 'Google ads', logo: gads },
+      { name: 'Meta Ads', logo: metaAds },
+      { name: 'Semrush', logo: semrush },
+      { name: 'Ahref Tool', logo: ahref },
+      { name: 'SEO', logo: seo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Green Pay pvt ltd",
+      date: "January 2023 - Present",
+      desc: "Developed and maintained web applications using PHP,MySQL, Bootstrap and JavaScript.integrated third-party APIs (e.g., payment gateways) toenhance functionality and user experience.Design dashboard with report and print functionality.Create WebView Apps in Android Using JavaUse data base SQlite for saving data .",
+      skills: [
+        "HTML",
+        "CSS",
+        "PHP",
+        "Mysql",
+        "JavaScript",
+        "Java",
+        "Ubuntu",
+        "Figma",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Php Developer",
+      company: "Digital Inspiron",
+      date: "april 2020 - October 2022",
+      desc: "building and maintaining web applications, gaining hands-on experience in PHP projects.Collaborated with front-end developers to integrate serverside logic with client-side scripts using JavaScript and jQuery.",
+      skills: [
+        "JavaScript",
+        "Bootstrap",
+        "HTML",
+        "CSS",
+        "Mysql",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Seo Expert",
+      company: "ComputeSol",
+      date: "May 2019 - March 2020",
+      desc: "develop on-page seo an analysis keyword research.knowledge of google search console and google analytics.implement backlinks e.g guest posting, infographics, pdf submition ,blog posting and profile creation.tools (ahref,semrush.google adward)",
+      skills: [
+        "ahref",
+        "semrush",
+        "google adward",
+        "Meta Ads",
+        "Google Analytics",
+        "Google Tag manager",
+        "keyword Research",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: superior,
+      school: "Superior University, Lahore",
+      date: "Sept 2014 - July 2018",
+      grade: "2.1 CGPA",
+      desc: "I have completed my bachelor degree (Bscs) in Computer Applications from Superior University, Lahore. During my time at SU, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Superior University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of science in Computer Science - BSCS",
+    },
+    {
+      id: 1,
+      img: college,
+      school: "Post Graduate Degree College, Bahawal Nagar",
+      date: "Sept 2012 - Aug 2014",
+      grade: "50.2%",
+      desc: "I completed my F.S.C in Science from Post Graduate Degree College, Bahawal Nagar. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of  Science.",
+      degree: "F.S.C - Post Graduate Degree College",
+    },
+  
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Ai Generated Course",
+      description:
+        " leverage AI technology to provide highly personalized learning experiences that help people achieve their educational goals.",
+      image: aiGen,
+      tags: ["Next Js", "Clerk", "Youtube Api"],
+      github: "https://github.com/Babarparacha/ai-generated-course",
+      webapp: "https://ai-generated-course.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "CS Prep",
+      description:
+        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/CSPrep",
+      webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/codingmastr/Movie-Recommendation-App",
+      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Email Validator NPM Package",
+      description:
+        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation"],
+      github: "https://github.com/codingmastr/cmtk-email-validator",
+      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },
+  ];  
