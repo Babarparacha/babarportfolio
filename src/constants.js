424cdd4039bcf35f64163c44d3a51b9155306523
babarparacha <babarmuzaffar3@gmail.com>
2025-06-18 16:03:28 +0500

    adding projects and minor changes

--- src/constants.js
+++ src/constants.js
@@ -37,6 +37,7 @@ import metaAds from './assets/tech_logo/metaAds.png';
 import seo from './assets/tech_logo/seo.png';
 import semrush from './assets/tech_logo/semrush.png';
 import ahref from './assets/tech_logo/ahref.png';
+import laravel from './assets/tech_logo/laravel.png';
 
 // Experience Section Logo's
 import webverseLogo from './assets/company_logo/webverse_logo.png';
@@ -61,6 +62,9 @@ import cmLogo from './assets/work_logo/cm.png';
 import imagesearchLogo from './assets/work_logo/image_search.png';
 import removebgLogo from './assets/work_logo/remove_bg.png';
 import aiGen from './assets/work_logo/aiGen.png';
+import mosque from './assets/work_logo/mosque.png';
+import plservice from './assets/work_logo/plservice.png';
+import mm from './assets/work_logo/mm.png';
 
 
 export const SkillsInfo = [
@@ -80,25 +84,26 @@ export const SkillsInfo = [
     ],
   },
   {
-    title: 'Backend',
+    title: 'Backend And DB',
     skills: [
-      { name: 'Node JS', logo: nodejsLogo },
-      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'Laravel', logo: laravel },
       { name: 'MySQL', logo: mysqlLogo },
       { name: 'MongoDB', logo: mongodbLogo },
-      { name: 'Firebase', logo: firebaseLogo },
-      { name: 'PostgreSQL', logo: postgreLogo },
-    ],
-  },
-  {
-    title: 'Languages',
-    skills: [
-      { name: 'Java', logo: javaLogo },
       { name: 'Python', logo: pythonLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+      // { name: 'Node JS', logo: nodejsLogo },
+      // { name: 'Express JS', logo: expressjsLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
     ],
   },
+  // {
+  //   title: 'Languages',
+  //   skills: [
+  //     { name: 'Java', logo: javaLogo },
+  //     { name: 'JavaScript', logo: javascriptLogo },
+  //     { name: 'TypeScript', logo: typescriptLogo },
+  //   ],
+  // },
   {
     title: 'Digital Marketing',
     skills: [
@@ -138,9 +143,10 @@ export const SkillsInfo = [
         "PHP",
         "Mysql",
         "JavaScript",
+        "Laravel",
         "Java",
-        "Ubuntu",
         "Figma",
+        // "Ubuntu",
       ],
     },
     {
@@ -212,82 +218,32 @@ export const SkillsInfo = [
     },
     {
       id: 1,
-      title: "CS Prep",
+      title: "MCF",
       description:
-        "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
-      image: csprepLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/codingmastr/CSPrep",
-      webapp: "https://csprep.netlify.app/",
+        "To establish an active Masjid, adhering to Islamic principles, with a welcoming environment, high-quality services.",
+      image: mosque,
+      tags: ["php", "", "HTML", "CSS", "JavaScript"],
+      // github: "https://github.com/codingmastr/CSPrep",
+      webapp: "https://beclixretail.online/mosque/index.html",
     },
     {
       id: 2,
-      title: "Movie Recommendation App",
-      description:
-        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
-      image: movierecLogo,
-      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/codingmastr/Movie-Recommendation-App",
-      webapp: "https://movie-recommendation-app-jet.vercel.app/",
+      title: "3PL Service",
+      // description:
+        // "Drop Shipping Plateform to create product and orders.",
+      image:plservice,
+      tags: ["php", "HTML", "CSS", "JavaScript"],
+      // github: "https://github.com/codingmastr/CSPrep",
+      webapp: "https://beclixretail.online/3pl_service/user/home/auth-login.html",
     },
     {
       id: 3,
-      title: "Email Validator NPM Package",
+      title: "Bclix Technology",
       description:
-        "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
-      image: npmLogo,
-      tags: ["React JS", "Node.js", "NPM", "Validation"],
-      github: "https://github.com/codingmastr/cmtk-email-validator",
-      webapp: "https://www.npmjs.com/package/cmtk-email-validator",
-    },
-    {
-      id: 4,
-      title: "Task Reminder Chrome Extension Tool",
-      description:
-        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
-      image: taskremLogo,
-      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-      github: "https://github.com/codingmastr/Task-Reminder-Tool",
-      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
-    },
-    {
-      id: 5,
-      title: "Webverse Digital",
-      description:
-        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-      image: webverLogo,
-      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-      github: "https://github.com/codingmastr/Webverse-Digital",
-      webapp: "https://webversedigital.com/",
-    },
-    {
-      id: 6,
-      title: "Coding Master",
-      description:
-        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-      image: cmLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-      github: "https://codingmasterweb.in/",
-      webapp: "https://codingmasterweb.in/",
-    },
-    {
-      id: 7,
-      title: "Image Search App",
-      description:
-        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-      image: imagesearchLogo,
-      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-      github: "https://github.com/codingmastr/Image-Search-App",
-      webapp: "https://imagsearch.netlify.app/",
-    },
-    {
-      id: 8,
-      title: "Image Background Remover",
-      description:
-        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-      image: removebgLogo,
-      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-      github: "https://github.com/codingmastr/Image-Background-Remover",
-      webapp: "https://removeyourbg.netlify.app/",
+        "plateform to connect shopify store with website.get orders and upload peoducts on shopify",
+      image:mm,
+      tags: ["php", "HTML", "CSS", "JavaScript","shopify"],
+      // github: "https://github.com/codingmastr/CSPrep",
+      webapp: "https://beclixretail.online/3pl_service/user/home/auth-login.html",
     },
   ];  
